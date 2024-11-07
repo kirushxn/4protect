68a6ef42fbd7800920003c6a9e06e972ebda1a7f
mwn <69548838+mawqin@users.noreply.github.com>
2024-11-07 12:48:09 +0100

    Update support.js

--- utilities/support.js
+++ utilities/support.js
@@ -14,7 +14,7 @@ module.exports = {
         const embed = new MessageEmbed()
             .setColor(color)
             .setDescription(`Tu veux rejoindre le support du bot ?
-            [Oui, je veux rejoindre](https://discord.gg/malaya)`)
+            [Oui, je veux rejoindre](https://discord.gg/U27kVRcwn3)`)
             .setFooter({ text: config.app.footer });
 
             await message.author.send({ embeds: [embed] });
